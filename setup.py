97fa5b9d64b8999d9e4f1b64d7d955eda10b8e4f
Aman Babu Hemant <amanbabuhemant@gmail.com>
2025-12-11 12:10:54 +0530

    Update setup, requirements and config file

--- setup.py
+++ setup.py
@@ -1,12 +1,95 @@
 import os
 import subprocess
+import json
+import shutil
+import importlib.util
+import urllib.request
 from random import randint
-from json import dumps
+from time import sleep
+from zipfile import ZipFile
+from textwrap import TextWrapper
 
 
-# command checking
+textwrapper = TextWrapper()
 
-print("checking commands/utilies")
+
+def segment():
+    print()
+    print("-" * 80)
+    print()
+    sleep(2)
+
+def center_line(line):
+    l = len(line)
+    if l > 80:
+        return line
+    padding = "-" * (40 - (l//2))
+    return padding + line + padding
+
+def indent_line(line, indent=1):
+    return ("    " * indent) + line
+
+def switch_color(color=""):
+    colors = {
+        "error": "\033[91m",
+        "warning": "\033[93m",
+        "success": "\033[92m",
+        "info": "\033[96m",
+        "reset": "\033[0m"
+    }
+    code = colors.get(color, colors["reset"])
+    print(code, end="", flush=True)
+
+def setup_print(text, color="", center=False, indent=0):
+    wrapped = textwrapper.wrap(text)
+    switch_color(color)
+    for line in wrapped:
+        if center:
+            line = center_line(line)
+        line = indent_line(line, indent)
+        print(line)
+    switch_color("reset")
+
+
+# setup
+
+setup_print("SETUP", "", True)
+segment()
+
+
+# config checking
+
+setup_print("CONFIG FILE", "info", True)
+
+if os.path.exists("config.json"):
+    config_file = open("config.json", "r")
+    try:
+        config_dict = json.load(config_file)
+    except:
+        setup_print("config.json found but invalid, deleting.", "error")
+        config_file.close()
+        os.remove("config.json")
+
+if not os.path.exists("config.json"):
+    print("config not found")
+    config_file = open("config.json", "w")
+    session_key = str(randint(100000, 999999))
+    config_dict = {
+        "SECRET_KEY": session_key,
+        "SERVER_NAME": "localhost:5000",
+    }
+    config_str = json.dumps(config_dict, indent=4)
+    config_file.write(config_str)
+    config_file.close()
+    setup_print("config.json generated", "success", indent=1)
+
+setup_print("DONE", "success", center=True)
+segment()
+
+
+# COMMANDS AND UTILITIES
+
+setup_print("COMMANDS/UTILITES", "info", True)
 
 def check_command(command):
     try:
@@ -20,13 +103,13 @@ def check_command(command):
     except:
         return False
 
-has_pip = check_command("pip")
-has_pip3 = check_command("pip3")
-has_python = check_command("python")
+has_pip     = check_command("pip")
+has_pip3    = check_command("pip3")
+has_python  = check_command("python")
 has_python3 = check_command("python3")
-has_git = check_command("git")
-has_docker = check_command("docker")
-has_gcc = check_command("gcc")
+has_git     = check_command("git") or check_command(config_dict.get("GIT_COMMAND_PATH", "git"))
+has_docker  = check_command("docker") or check_command(config_dict.get("DOCKER_COMMEND_PATH", "docker"))
+has_gcc     = check_command("gcc") or check_command(config_dict.get("GCC_COMMAND_PATH", "gcc"))
 
 pip_command = python_command = ""
 
@@ -43,100 +126,146 @@ pip_command = "pip" if has_pip else "pip3"
 python_com3mand = "python" if has_python else "python3"
 
 if not has_pip and not has_pip3:
-    print("Faild to find the pip command")
-    print("pip is required for packages installation")
-    print("exiting...")
-    exit()
+    setup_print("Faild to find the pip command", "error")
+    setup_print("pip is required for packages installation", "error")
+    setup_print("exiting...", "error")
+    exit(1)
 else:
-    print("\thas pip")
+    setup_print("has pip", "success", indent=1)
 
 if not has_python and not has_python3:
     print("Warning: Failed to find python")
+    setup_print("Faild to find python command", "error")
+    setup_print("How did this script ran?", "error")
+    setup_print("exiting...", "error")
+    exit(1)
 else:
-    print("\thas python")
+    setup_print("has python", "success", indent=1)
 
-if not has_git: #and not has_git_from_config:
-    print("Warning: Failed to find git")
+if has_git:
+    setup_print("has get", "success", indent=1)
 else:
-    print("\thas get")
+    setup_print("Failed to find git", "warning")
 
-if not has_docker: #and not has_docker_from_config_path:
-    print("Warning: Failed to find docker")
+if has_docker:
+    setup_print("has docker", "success", indent=1)
 else:
-    print("\thas docker")
+    setup_print("Failed to find docker", "warning")
 
-print()
-
-
-# requirements
-
-print("installing requirements")
+setup_print("DONE", "success", center=True)
+segment()
 
 
-subprocess.run(
-    [pip_command, "install", "-r", "requirements.txt"],
-    stdout = subprocess.PIPE,
-)
+# REQUIREMENTS
 
-print("\tinstalled requirements")
-print()
+setup_print("REQUIREMENTS", "info", True)
 
+try:
+    requirements = open("requirements.txt").read().split()
+except:
+    setup_print("unable to read requirements", "error")
+    setup_print("exiting...", "error")
+    exit(1)
 
-# directories
+for requirement in requirements:
+    spec = importlib.util.find_spec(requirement)
+    if spec:
+        setup_print("Requirement satisfied: " + requirement, "success", indent=1)
+        continue
 
-print("creating directories")
+    setup_print("Instaling " + requirement)
+    output = subprocess.run(
+        [pip_command, "install", requirement],
+        capture_output=True,
+        text=True,
+    )
 
-for r_dir in ["files", "files/blob", "files/dp", "files/tmp", "files/qr", "instance"]:
-    if not os.path.exists(r_dir):
-        os.mkdir(r_dir)
-        print("created directory", r_dir)
+    if output.returncode:
+        setup_print("some error happen while instaling requirements", "error")
+        setup_print("")
+        setup_print(output.stderr, "error")
+        exit(1)
 
-print("\tdirectories created")
-print()
+    setup_print("Installed " + requirement, "success", indent=1)
 
-
-# database
-
-print("creating database")
-
-from app import app, db
-
-with app.app_context():
-    db.create_all()
-
-print("\tdatabase created")
-print()
-
-
-# config
-
-print("checking config file")
-
-if not os.path.exists("config.json"):
-    print("config not found")
-    config_file = open("config.json", "w")
-    session_key = str(randint(100000, 999999))
-    config_dict = {
-        "SECRET_KEY": session_key,
-        "SERVER_NAME": "localhost:5000",
-    }
-    config_str = dumps(config_dict, indent=4)
-    config_file.write(config_str)
-    config_file.close()
-    print("\tconfig.json generated")
-    print()
-
-print()
+setup_print("DONE", "success", center=True)
+segment()
 
 
-# end
+# DIRECTORIES
 
-print("The environment is ready for development")
-print("Press enter to exit, r for run the app")
+setup_print("DIRECTORIES", "info", True)
 
-i = input("[enter/r]: ").lower()
+for r_dir in [
+        ["app", "static", "vendor"],
+        ["files"],
+        ["files", "blob"],
+        ["files", "dp"],
+        ["files", "tmp"],
+        ["files", "qr"],
+        ["instance"],
+    ]:
+    d = os.path.join(*r_dir)
+    if not os.path.exists(d):
+        os.mkdir(d)
+        setup_print("Directory created: " + d, "success", indent=1)
+    else:
+        setup_print("Directory exists: " + d, "success", indent=1)
+
+setup_print("DONE", "success", center=True)
+segment()
+
+
+# VENDORS
+
+tmp_dir = os.path.join("files", "tmp")
+codemirror_dir = os.path.join("app", "static", "vendor", "codemirror")
+
+if os.path.exists(codemirror_dir):
+    setup_print("Vendor exists: codemirror", "success", indent=1)
+else:
+    setup_print("Setting up codemirror")
+    setup_print("Downloading codemirror")
+    try:
+        codemirror_zip_path, _ = urllib.request.urlretrieve("https://codemirror.net/5/codemirror.zip")
+    except Exception as e:
+        setup_print("Error while downoalding", "error")
+        setup_print(str(e), "error")
+        setup_print("exiting...", "error")
+        exit(1)
+
+    setup_print("Setting up codemirror")
+    setup_print("Downloading codemirror")
+    try:
+        codemirror_tmp_dir = os.path.join(tmp_dir, "codemirror")
+        try:
+            shutil.rmtree(codemirror_tmp_dir)
+        except:
+            pass
+        codemirror_zip = ZipFile(codemirror_zip_path, "r")
+        codemirror_zip.extractall(codemirror_tmp_dir)
+        main_dir = os.listdir(codemirror_tmp_dir)[0]
+        shutil.move(os.path.join(codemirror_tmp_dir, main_dir), codemirror_dir)
+        shutil.rmtree(codemirror_tmp_dir)
+    except Exception as e:
+        setup_print("Error while extracting", "error")
+        setup_print(str(e), "error")
+        setup_print("exiting...", "error")
+        exit(1)
+
+    setup_print("Setup vendor: codemirror", "success", indent=1)
+
+
+# END
+
+setup_print("The environment is ready for development", "success", indent=1)
+setup_print("Press enter to exit, r for run the app", indent=1)
+
+i = input("    [<enter>/r]: ").lower()
 
 if i == "r":
     subprocess.run(
         [python_command, "run.py"],
     )
+
+setup_print("DONE", "success", True)
