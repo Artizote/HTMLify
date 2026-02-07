157c856bba2e88649fae5c4b704696135d39d967
Aman Babu Hemant <amanbabuhemant@gmail.com>
2026-02-08 03:58:55 +0530

    Minor change in embed endpoint

--- app/routes/api/public/embed.py
+++ app/routes/api/public/embed.py
@@ -1,7 +1,7 @@
 from flask import request, render_template
 from pygments.formatters import HtmlFormatter
 
-from app.models import File, FileType, FileVisibility
+from app.models import File, BlobType, FileVisibility
 
 from .api import public_api
 
@@ -11,7 +11,7 @@ from .api import public_api
 def embed():
     id = request.args.get("id", 0, int)
     file = File.by_id(id)
-    if not file or file.type != FileType.TEXT or file.visibility != FileVisibility.PUBLIC:
+    if not file or file.blob.type != BlobType.TEXT or file.visibility != FileVisibility.PUBLIC:
         return "file not found", 404
     return render_template("embed.html", highlight_style=HtmlFormatter().get_style_defs(), file=file)
 
