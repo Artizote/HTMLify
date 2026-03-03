84e3d9fe191a172078648ccb762d0264f7dc1789
Aman Babu Hemant <amanbabuhemant@gmail.com>
2026-03-03 20:37:54 +0530

    Add uvicorn run fur running backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import uvicorn
 
 from .api.v1.api import router as v1_router
 
 app = FastAPI(title="HTMLify")
 
 app.include_router(v1_router, prefix="/v1")
+
+def run_app():
+    uvicorn.run(app, host="api.localhost", port=8000)
+
+if __name__ == "__main__":
+    run_app()
---
+++ backend/app/__main__.py
@@ -0,0 +1,3 @@
+from . import run_app
+
+run_app()
